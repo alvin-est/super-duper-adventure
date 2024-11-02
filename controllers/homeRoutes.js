7a633893832b0aab4d9aa275afa587771a684adb
alvin-est <estado.alvin@gmail.com>
2024-11-03 02:11:46 +1100

    modified files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,31 @@
 const router = require('express').Router();
+const authorisedOnly = require('../utils/auth');
 
 // Home page
 router.get('/', async (req, res) => {
     console.log("Rendering home page.");
-    res.render('pages/homepage');
+    res.render('pages/homepage', {
+        logged_in: req.session.logged_in,
+        username: req.session.username
+    });
 });
 
 // Dashboard
-router.get('/dashboard', async (req, res) => {
+router.get('/dashboard', authorisedOnly, async (req, res) => {
     console.log("Rendering dashboard.");
     res.render('pages/dashboard');
+});
+
+// Login
+router.get('/login', async (req, res) => {
+    console.log("Rendering login page.");
+    res.render('pages/login');
+});
+
+// Register
+router.get('/register', async (req, res) => {
+    console.log("Rendering register page.");
+    res.render('pages/register');
 });
 
 module.exports = router;
---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// Redirect user if not logged in
+const auth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+module.exports = auth;
