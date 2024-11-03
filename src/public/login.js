d12f7b158248b2cc28706c36f2674b89c9162573
alvin-est <estado.alvin@gmail.com>
2024-11-03 12:28:59 +1100

    change login landing to dashboard

--- src/public/login.js
+++ src/public/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // alert("Succesfully logged in!");  /* include or not */
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert('Failed to log in');
       }
